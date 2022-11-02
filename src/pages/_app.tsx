4977cfec6cd402987e522483c3ad33f5870f8c06
Icaro Ramos <polosnramos@hotmail.com>
2022-11-02 17:08:45 -0300

    feat: styles index form

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return;
-  <ChakraProvider theme={theme}>
-    <Component {...pageProps} />;
-  </ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
